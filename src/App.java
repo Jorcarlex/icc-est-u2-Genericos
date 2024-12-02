5bb16fef022a5654a59337d0e901a52e671dd2d0
unknown <Jorcarlex>
2024-12-02 15:33:34 -0500

    second commit

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import models.genericos.Persona;
 public class App {
     public static void main(String[] args) throws Exception {
         Caja<String> cajaString = new Caja<>();
-        Caja<Integer> CajaInteger = new Caja<>();
+        Caja<Integer> cajaInteger = new Caja<>();
         Caja<Persona> cajaPerson = new Caja<>();
 
         Persona persona = new Persona();
@@ -13,13 +13,13 @@ public class App {
         persona.setEdad(19);
 
         cajaString.guardar("Guardar un String");
-        CajaInteger.guardar(100);
+        cajaInteger.guardar(100);
         cajaPerson.guardar(persona);
         // Para crear una persona
         /// cajaPerson.guardar(new Persona());
 
         System.out.println(cajaString.obtener());
-        System.out.println(CajaInteger.obtener());
+        System.out.println(cajaInteger.obtener());
         System.out.println(cajaPerson.obtener().getNombre());
 
         Par<Integer, String> par = new Par<>();
@@ -39,24 +39,27 @@ public class App {
 
         ////////////////////////////////////////////////////////
 
+        System.out.println(" ");
+        System.out.println("////////////////////////////////////////////////");
         Persona[] personas = new Persona[10];
-        personas[0] = new Persona("Jordan", 10);
+        personas[0] = new Persona("Jordan", 14);
         personas[1] = new Persona("Brandom", 20);
-        personas[3] = new Persona("Jose", 23);
-        personas[4] = new Persona("Pepe", 39);
-        personas[5] = new Persona("Kate", 40);
-        personas[6] = new Persona("Sofia", 66);
-        personas[7] = new Persona("Liz", 23);
-        personas[8] = new Persona("Alex", 10);
-        personas[9] = new Persona("Camila", 5);
-        personas[10] = new Persona("Juan", 7);
-        
+        personas[2] = new Persona("Jose", 23);
+        personas[3] = new Persona("Pepe", 39);
+        personas[4] = new Persona("Kate", 1);
+        personas[5] = new Persona("Sofia", 66);
+        personas[6] = new Persona("Liz", 23);
+        personas[7] = new Persona("Alex", 10);
+        personas[8] = new Persona("Camila", 5);
+        personas[8] = new Persona("Mika", 2);
+
         int personasMayores = 0;
         int personasMenores = 0;
-        for (int i = 0; i < personas.length; i++) {
-            if (personas[i].getEdad() >= 18) {
+
+        for (Persona p : personas) {
+            if (p != null && p.getEdad() >= 18) {
                 personasMayores++;
-            }else {
+            } else if (p != null) {
                 personasMenores++;
             }
         }
@@ -67,18 +70,33 @@ public class App {
         int contadorMayores = 0;
         int contadorMenores = 0;
 
-        for (int i = 0; i < personas.length; i++) {
-            if (personas[i].getEdad() >= 18) {
-                mayores[contadorMayores] = new Par<String, Integer>();
-                mayores[contadorMayores].establecerClave(personas[i].getNombre());
-                mayores[contadorMayores].establecerValor(personas[i].getEdad());
-                contadorMayores++;
-            }else {
-                mayores[contadorMenores] = new Par<Integer, String>();
-                mayores[contadorMenores].establecerClave(personas[i].getEdad());
-                mayores[contadorMenores].establecerValor(personas[i].getNombre());
-                contadorMenores++;
+        for (Persona p : personas) {
+            if (p != null) {
+                if (p.getEdad() >= 18) {
+                    mayores[contadorMayores] = new Par<>();
+                    mayores[contadorMayores].establecerClave(p.getNombre());
+                    mayores[contadorMayores].establecerValor(p.getEdad());
+                    contadorMayores++;
+                } else {
+                    menores[contadorMenores] = new Par<>();
+                    menores[contadorMenores].establecerClave(p.getEdad());
+                    menores[contadorMenores].establecerValor(p.getNombre());
+                    contadorMenores++;
+                }
             }
+        }
+
+        System.out.println(" ");
+        System.out.println("MAYORES DE EDAD:");
+        for (Par<String, Integer> p : mayores) {
+            System.out.println(p.obtenerClave() + " - " + p.obtenerValor());
+        }
+
+        System.out.println(" ");
+
+        System.out.println("MENORES DE EDAD:");
+        for (Par<Integer, String> p : menores) {
+            System.out.println(p.obtenerClave() + " - " + p.obtenerValor());
         }
 
     }
